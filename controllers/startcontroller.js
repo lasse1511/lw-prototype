8c2b71f1f7b6f28eae37e08c2f55918a6e72f6e6
danielbuhl <daniel@buhl.nu>
2019-10-13 11:28:25 +0200

    createOrder

--- controllers/startcontroller.js
+++ controllers/startcontroller.js
@@ -3,6 +3,10 @@ exports.getStart = function(req, res) {
     res.render('start');
 }
 
+exports.getCreateOrder = function(req, res) {
+    res.render('createOrder');
+}
+
 exports.getHome = function(req, res) {
     res.end('Hello world')
 }
