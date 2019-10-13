13255a98e3f94cc3841e87587c9621a329e1868c
Lasse Bonner <lassebonner@hotmail.com>
2019-10-13 14:20:20 +0200

    Addition of logo and script

--- public/scripts/start.js
+++ public/scripts/start.js
@@ -2,4 +2,9 @@ $(document).ready(function () {
     $('#addbtnbtn').click(function () {
         $('#labeldiv').append('<label> new label </label>')
     })
+
+
+    $('#userBtn').click(function () {
+        window.location = '/createOrder'
+    })
 })
