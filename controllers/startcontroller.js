88afccd0ebac9140b4e893aa85e4fd652fca4688
pandagud <pandagud@gmail.com>
2019-10-13 11:38:36 +0200

    MergeConflit

--- controllers/startcontroller.js
+++ controllers/startcontroller.js
@@ -2,6 +2,9 @@
 exports.getStart = function(req, res) {
     res.render('start');
 }
+exports.getSearch = function(req,res){
+    res.render('search');
+}
 
 exports.getCreateOrder = function(req, res) {
     res.render('createOrder');
--- routes/start.js
+++ routes/start.js
@@ -2,5 +2,8 @@ module.exports = function(app) {
     var startcontroller = require("../controllers/startcontroller")
     app.route('/start').get(startcontroller.getStart)
     app.route('/createOrder').get(startcontroller.getCreateOrder)
+
+
+    app.route('/search').get(startcontroller.getSearch);
     app.route('/').get(startcontroller.getHome)
 }
