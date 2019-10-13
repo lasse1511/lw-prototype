0f09c506c50becf3fef8e22ff8475326f11ac186
danielbuhl <daniel@buhl.nu>
2019-10-13 16:06:07 +0200

    search job table

--- public/scripts/searchResult.js
+++ public/scripts/searchResult.js
@@ -1,14 +1,20 @@
-var url = "/";
+var url = "/getJobs";
 $(document).ready(function () {
     $('#searchbtn').click(function () {
-        $('#resultul').empty();
         $.get(url, null, function (foundJobs) {
-            $('#resultul').empty();
-            // foundJobs.forEach(element => {
-            //     $('#resultul ul li:last').append(
-            //         "<li>hej</li> "
-            //     )
-            // });
+            $('#resulttable').empty();
+            var tableHeader = "<thead><tr><th scope='col-md-2'>Opsamling</th><th scope='col-md-2'>Destination</th><th scope='col-md-2'>Gods</th><th scope='col-md-2'>Specielle Krav</th><th></th></tr></thead>"
+            $('#resulttable').append(tableHeader);
+            foundJobs.forEach(job => {
+                var tr="<tbody><tr>"
+                tr = tr+"<td scope='row'><div class='pickup'>"+job.pickUpLocation+"</div><div>"+job.pickUpDate+"</div></td>"
+                tr = tr+"<td scope='row'><div class='destination'>"+job.destination+"</div><div>"+job.ETADate+"</div></td>"
+                tr = tr+"<td scope='row'><div class='sizeAndQuantity'>"+job.quantity+"</div></td>"
+                tr = tr+"<td scope='row'><div class='special'></div></td>"
+                tr = tr+"<td scope='row'><button class='custombtn'>Afgiv Bud</button></td>"
+                tr = tr+"</tr></tbody>"  
+                $('#resulttable').append(tr);
+            });
         });
 
     })
