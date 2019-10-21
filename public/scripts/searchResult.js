933086fe0c2b460a4b3d160a1e96b687aaa8a921
Lasse Bonner <lassebonner@hotmail.com>
2019-10-21 18:03:29 +0200

    click the tbody to show modal

--- public/scripts/searchResult.js
+++ public/scripts/searchResult.js
@@ -2,23 +2,22 @@ $(document).ready(function () {
     $('#searchbtn').click(function () {
         $.get("/getJobs", null, function (foundJobs) {
             $('#resulttable').empty();
-            var tableHeader = "<thead><tr><th scope='col-md-2'>Opsamling</th><th scope='col-md-2'>Destination</th><th scope='col-md-2'>Gods</th><th scope='col-md-2'>Specielle Krav</th><th></th></tr></thead>"
+            var tableHeader = "<thead><tr><th scope='col-md-2'>Opsamling</th><th scope='col-md-2'>Destination</th><th scope='col-md-2'>Gods</th><th scope='col-md-2'>Specielle Krav</th></tr></thead>"
             $('#resulttable').append(tableHeader);
             foundJobs.forEach(job => {
-                var tr = "<tbody id=" + job.id + "><tr>"
-                tr = tr + "<td colspan='5'><h5>" + job.owner + " Ordre: " + job.id + "</h5></td></tr><tr class='tableBody'>"
+                var tr = "<tbody data-toggle='modal' data-target='#bidModal' id=" + job.id + "><tr>"
+                tr = tr + "<td colspan='4'><h5>" + job.owner + " Ordre: " + job.id + "</h5></td></tr><tr class='tableBody'>"
                 tr = tr + "<td scope='row'><div class='pickup'>" + job.pickUpLocation + "</div><div>" + job.pickUpDate + "</div></td>"
                 tr = tr + "<td scope='row'><div class='destination'>" + job.destination + "</div><div>" + job.ETADate + "</div></td>"
                 tr = tr + "<td scope='row'><div class='sizeAndQuantity'>" + job.quantity + "</div></td>"
-                tr = tr + "<td scope='row'><div class='special'></div></td>"
-                tr = tr + "<td scope='row'><button class='customBtn btn btn-primary' type='button' data-toggle='modal' data-target='#bidModal' data-jobowner=" + job.owner + " data-jobid=" + job.id + ">Afgiv Bud</button></td>"
+                tr = tr + "<td scope='row'><div class='special'></div>"
+                // tr = tr + "<td scope='row'>"
+                tr = tr + "</td>"
                 tr = tr + "</tr></tbody>"
                 $('#resulttable').append(tr);
             });
         });
-
     })
-
 
     $('#bidModal').on('show.bs.modal', function (event) {
         var button = $(event.relatedTarget) // Button that triggered the modal
@@ -37,3 +36,21 @@ $(document).ready(function () {
         });
     });
 })
+
+// $(document).on('DOMNodeInserted', function (e) {
+
+//     //Adds click handlers on the tbody class
+//     var tbodys = document.getElementsByClassName('tbody')
+//     for (let i = 0; i < tbodys.length; i++) {
+//         tbodys[i].removeEventListener('click', function (e) {})
+//         tbodys[i].addEventListener('click', function () {
+//             handleClick(e)
+//         });
+//     };
+
+//     function handleClick(e) {
+//         $('#tbtn'+e.target.id).click()
+//         console.log(e.target.id)
+//     }
+
+// });
